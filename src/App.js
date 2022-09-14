d733a205fbc29019ed0d70f174675b6bcc85e262
greenwhale00 <goreakay@gmail.com>
2022-09-14 17:19:49 +0900

    220914

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
 import './App.css';
+import './component/Header';
+import './component/MainVisual'
 
 function App() {
   return (
-   
+    <div className="Wrap">
+      <Header />
+      {/* <main>
+        <MainVisual />
+      </main> */}
+    </div>
   );
 }
 
